923fedb63b883a057238d8230ef0266a3fe965b4
Bharath H M <135454305+BrainToBytes@users.noreply.github.com>
2025-10-11 10:54:19 +0530

    Time: 1 ms (99.72%), Space: 41.8 MB (69.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+               stack[++top]= ch;
+            }
+            else
+            {
+                if(top == -1) return false;
+
+                char topChar = stack[top];
+                if((ch == ')' && topChar == '(') || (ch == '}' && topChar == '{') ||
+                    (ch == ']' && topChar == '['))
+                    {
+                        top--;
+                    }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return top == -1;
+    }
+}
